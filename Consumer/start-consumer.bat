73d98039cd719f28b3582f54cf619fb7a0d90c39
Eduardo Aguiar <eduardoaguiar@positivosolucoes.com.br>
2022-12-09 08:55:21 -0300

    #NOTASK Console

---
+++ Consumer/start-consumer.bat
@@ -0,0 +1,1 @@
+dotnet run "localhost:9092" "server1.testDB.dbo.Clientes"
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Serilog;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            logger.Information("Testando o consumo de mensagens com Kafka");
