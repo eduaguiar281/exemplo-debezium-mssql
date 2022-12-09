c1b2209f3205c951011798327cf48bfb3b9ac670
Eduardo Aguiar <eduardoaguiar@positivosolucoes.com.br>
2022-12-09 10:22:56 -0300

    Removendo arquivos

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Serilog;
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-var logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .CreateLogger();
-            logger.Information("Testando o consumo de mensagens com Kafka");
